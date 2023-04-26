cdffcf6507bbc01664d49af51f535bb95d078fb8
Abdullah Köseliören <abdullahoren1@gmail.com>
2023-04-26 13:32:12 +0300

    Added EF Core.

---
+++ WebApi/Controllers/ProductController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Repositories;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            try
+            {
+                var products = _context.Products.ToList();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneProducts([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var product = _context.Products.Where(p => p.Id.Equals(id)).SingleOrDefault();
+
+                if (product is null)
+                {
+                    return NotFound();
+                }
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            try
+            {
+                if (product is null)
+                {
+                    return BadRequest();
+                }
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return StatusCode(201, product);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateProduct([FromRoute(Name = "id")] int id, [FromBody] Product product)
+        {
+            try
+            {
+                var result = _context.Products.Where(p => p.Id.Equals(id)).SingleOrDefault();
+
+                if (result is null)
+                {
+                    return NotFound(); //404 Status code
+                }
+
+                if (id != product.Id)
+                {
+                    return BadRequest(); //400 Status code
+                }
+
+                result.Title = product.Title;
+                result.Price = product.Price;
+
+                _context.SaveChanges();
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteProducts([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var result = _context.Products.Where(p => p.Id.Equals(id)).SingleOrDefault();
+                if (result is null)
+                {
+                    return NotFound(); //404 Status code
+                }
+
+                _context.Products.Remove(result);
+                _context.SaveChanges();
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ WebApi/Repositories/Config/ProductConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Repositories.Config
+{
+    public class ProductConfig : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product { Id = 1, Title = "Buzdolabı", Price = 275 },
+                new Product { Id = 2, Title = "Çamaşır Makinası", Price = 250 },
+                new Product { Id = 3, Title = "Bulaşık Makinası", Price = 200 }
+                );
+        }
+    }
+}
